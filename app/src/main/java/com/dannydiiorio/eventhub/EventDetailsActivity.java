3671c42546501068bacc722ac150f6583e72616a
Daniel DiIorio <ddiiorio@my.bcit.ca>
2019-06-18 12:59:04 -0700

    Change from Place Picker to Places AutoComplete

--- app/src/main/java/com/dannydiiorio/eventhub/EventDetailsActivity.java
+++ app/src/main/java/com/dannydiiorio/eventhub/EventDetailsActivity.java
@@ -21,20 +21,23 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.dannydiiorio.eventhub.Model.Event;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.android.libraries.places.compat.GeoDataClient;
-import com.google.android.libraries.places.compat.Place;
-import com.google.android.libraries.places.compat.PlaceBufferResponse;
-import com.google.android.libraries.places.compat.Places;
-import com.google.android.libraries.places.compat.ui.PlacePicker;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.RectangularBounds;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 import com.google.firebase.database.DatabaseReference;
 import com.squareup.picasso.Picasso;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -43,11 +46,18 @@ import static com.dannydiiorio.eventhub.App.Constants.eventsAll;
 import static com.dannydiiorio.eventhub.App.Constants.eventsUser;
 import static com.dannydiiorio.eventhub.App.Constants.vibe;
 
+//import com.google.android.libraries.places.compat.GeoDataClient;
+//import com.google.android.libraries.places.compat.Place;
+//import com.google.android.libraries.places.compat.PlaceBufferResponse;
+//import com.google.android.libraries.places.compat.Places;
+//import com.google.android.libraries.places.compat.ui.PlacePicker;
+
 public class EventDetailsActivity extends AppCompatActivity {
     private static final int PLACE_PICKER_REQUEST = 1;
+    private static final int AUTOCOMPLETE_REQUEST_CODE = 1;
     private static final String TAG = "LOGTAG";
-    private static final double LAT = 49.316054;
-    private static final double LON = -123.026416;
+    private static double LAT = 49.316054;
+    private static double LON = -123.026416;
     private TextView eventVenue;
     private TextView venueAddress;
     private TextView eventTitle;
@@ -59,7 +69,8 @@ public class EventDetailsActivity extends AppCompatActivity {
     private String id = null;
     private String ticketLink = null;
     private boolean isCustom;
-    protected GeoDataClient mGeoDataClient;
+    //protected GeoDataClient mGeoDataClient;
+    private FusedLocationProviderClient fusedLocationClient;
     protected DatabaseReference db;
     protected Event current;
     protected int currentIndex;
@@ -80,7 +91,8 @@ public class EventDetailsActivity extends AppCompatActivity {
                 .placeholder(R.drawable.empty_profile)
                 .into(profile);
 
-        mGeoDataClient = Places.getGeoDataClient(this, null);
+        //mGeoDataClient = Places.getGeoDataClient(this, null);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         eventImage = findViewById(R.id.eventImage);
         tmLogo = findViewById(R.id.ticketmasterLogo);
@@ -111,18 +123,18 @@ public class EventDetailsActivity extends AppCompatActivity {
                 eventDate.setText(current.getEventDate());
                 ImageHelper.loadImage(current.getImgUrl(), eventImage);
                 String placeId = intent.getStringExtra("placeId");
-                if (placeId != null) {
-                    mGeoDataClient.getPlaceById(placeId)
-                            .addOnCompleteListener(task -> {
-                                if (task.isSuccessful()) {
-                                    PlaceBufferResponse places = task.getResult();
-                                    Place myPlace = places.get(0);
-                                    eventVenue.setText(myPlace.getName());
-                                    venueAddress.setText(myPlace.getAddress());
-                                    places.release();
-                                }
-                            });
-                }
+//                if (placeId != null) {
+//                    mGeoDataClient.getPlaceById(placeId)
+//                            .addOnCompleteListener(task -> {
+//                                if (task.isSuccessful()) {
+//                                    PlaceBufferResponse places = task.getResult();
+//                                    Place myPlace = places.get(0);
+//                                    eventVenue.setText(myPlace.getName());
+//                                    venueAddress.setText(myPlace.getAddress());
+//                                    places.release();
+//                                }
+//                            });
+//                }
                 if (intent.getStringExtra("venueName") != null) {
                     eventVenue.setText(intent.getStringExtra("venueName"));
                 }
@@ -145,9 +157,32 @@ public class EventDetailsActivity extends AppCompatActivity {
         if (isCustom) {
             tmLogo.setVisibility(View.GONE);
             ticketUrl.setVisibility(View.GONE);
+//            eventVenue.setOnClickListener(v -> {
+//                LatLng neCorner = new LatLng(LAT, LON); //49.316054, -123.026416
+//                LatLng swCorner = new LatLng(LAT-0.0976, LON-0.1888);
+//                if (ActivityCompat.checkSelfPermission(EventDetailsActivity.this,
+//                        Manifest.permission.ACCESS_FINE_LOCATION)
+//                        != PackageManager.PERMISSION_GRANTED) {
+//                    ActivityCompat.requestPermissions(EventDetailsActivity.this,
+//                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+//                            PLACE_PICKER_REQUEST);
+//                    return;
+//                }
+//                try {
+//                    PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+//                    builder.setLatLngBounds(new LatLngBounds(swCorner, neCorner));
+//                    Intent i = builder.build(EventDetailsActivity.this);
+//                    startActivityForResult(i, PLACE_PICKER_REQUEST);
+//                } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+//                    Log.e(TAG, String.format("GooglePlayServices Not Available [%s]", e.getMessage()));
+//                } catch (Exception e) {
+//                    Log.e(TAG, String.format("PlacePicker Exception: %s", e.getMessage()));
+//                }
+//            });
+
+            Places.initialize(getApplicationContext(), getResources().getString(R.string.google_api_key));
+
             eventVenue.setOnClickListener(v -> {
-                LatLng neCorner = new LatLng(LAT, LON); //49.316054, -123.026416
-                LatLng swCorner = new LatLng(LAT-0.0976, LON-0.1888);
                 if (ActivityCompat.checkSelfPermission(EventDetailsActivity.this,
                         Manifest.permission.ACCESS_FINE_LOCATION)
                         != PackageManager.PERMISSION_GRANTED) {
@@ -157,14 +192,24 @@ public class EventDetailsActivity extends AppCompatActivity {
                     return;
                 }
                 try {
-                    PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-                    builder.setLatLngBounds(new LatLngBounds(swCorner, neCorner));
-                    Intent i = builder.build(EventDetailsActivity.this);
-                    startActivityForResult(i, PLACE_PICKER_REQUEST);
-                } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
-                    Log.e(TAG, String.format("GooglePlayServices Not Available [%s]", e.getMessage()));
+                    fusedLocationClient.getLastLocation()
+                            .addOnSuccessListener(this, location -> {
+                                // Got last known location. In some rare situations this can be null.
+                                if (location != null) {
+                                    LAT = location.getLatitude();
+                                    LON = location.getLongitude();
+                                }
+                            });
+                    List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME);
+                    Intent intent = new Autocomplete.IntentBuilder(
+                            AutocompleteActivityMode.FULLSCREEN, fields)
+                            .setLocationBias(RectangularBounds.newInstance(
+                                    new LatLng(LAT-0.08, LON-0.12),
+                                    new LatLng(LAT+0.08, LON+0.12)))
+                            .build(this);
+                    startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
                 } catch (Exception e) {
-                    Log.e(TAG, String.format("PlacePicker Exception: %s", e.getMessage()));
+                    Log.e(TAG, String.format("Places Exception: %s", e.getMessage()));
                 }
             });
 
@@ -223,19 +268,37 @@ public class EventDetailsActivity extends AppCompatActivity {
         saveEvent();
     }
 
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == PLACE_PICKER_REQUEST) {
+//            if (resultCode == RESULT_OK) {
+//                final Place place = PlacePicker.getPlace(data, this);
+//                eventVenue.setText(place.getName());
+//                venueAddress.setText(place.getAddress());
+//                current.setPlaceId(place.getId());
+//                current.setVenueAddress(String.valueOf(place.getAddress()));
+//                current.setVenueName(String.valueOf(place.getName()));
+//                saveEvent();
+//                String venueConfirm = getResources().getString(R.string.venueConfirm);
+//                Snackbar.make(findViewById(android.R.id.content),
+//                        venueConfirm, Snackbar.LENGTH_LONG).show();
+//            }
+//        }
+//    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == PLACE_PICKER_REQUEST) {
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
-                final Place place = PlacePicker.getPlace(data, this);
+                Place place = Autocomplete.getPlaceFromIntent(data);
                 eventVenue.setText(place.getName());
                 venueAddress.setText(place.getAddress());
-                current.setPlaceId(place.getId());
-                current.setVenueAddress(String.valueOf(place.getAddress()));
-                current.setVenueName(String.valueOf(place.getName()));
-                saveEvent();
-                String venueConfirm = getResources().getString(R.string.venueConfirm);
-                Snackbar.make(findViewById(android.R.id.content),
-                        venueConfirm, Snackbar.LENGTH_LONG).show();
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+                // TODO: Handle the error.
+                Status status = Autocomplete.getStatusFromIntent(data);
+                Log.i(TAG, status.getStatusMessage());
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
             }
         }
     }
